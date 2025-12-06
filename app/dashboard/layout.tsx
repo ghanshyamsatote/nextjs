4e3b8882f51ba6bfd844827dd9c7cce0304ed53c
ghanshyamsatote <ghanshyamsatote2002@gmail.com>
2025-12-06 17:54:33 +0530

    Initial Next.js project

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,13 @@
+import SideNav from "@/app/ui/dashboard/sidenav";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+  <div className="w-full flex-none md:w-64 h-16 md:h-full">
+    <SideNav />
+  </div>
+  <div className="grow p-6 md:overflow-y-auto md:p-12 h-full">{children}</div>
+</div>
+
+  );
+}
---
+++ app/ui/acme-logo.tsx
@@ -0,0 +1,11 @@
+import { GlobeAltIcon } from '@heroicons/react/24/outline';
+
+export default function AcmeLogo() {
+  return (
+   <div className="lusitana_block">
+  <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
+  <span className="text-[44px]">Acme</span>
+</div>
+
+  );
+}
